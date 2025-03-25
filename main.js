6d53de7458b09942891fc2fd9f12a3eedf10f54e
samumuku <pr13xpj@eduvaud.ch>
2025-03-25 15:40:01 +0100

    step 1-2-3 done

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Vue Gadget Shop",
+      description:
+        "Découvrez les derniers gadgets électroniques de haute technologie !",
+      name: "Smartphone XZ",
+      price: 799,
+      image: "./assets/phone.jpg",
+      inStock: true,
+    };
+  },
+});
